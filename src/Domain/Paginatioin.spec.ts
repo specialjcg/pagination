e71ab070756df7d1256e61a8c91b9567821fb9c2
jcgouleau <jean-charles.gouleau@savoye.com>
2020-09-22 11:50:32 +0200

    <Feat>transform to functionnal programming

--- src/Domain/Paginatioin.spec.ts
+++ src/Domain/Paginatioin.spec.ts
@@ -1,61 +1,80 @@
-const setPagination = (list: number[], index: number) => {
-  const pageSelect = '(' + list[index] + ')';
-  const startInfSeven = [...list.slice(0, index)];
-  const startSupSeven = [1, '...'];
-  const lastPageInfSeven = [...list.slice(index + 1, list.length)];
-  const lastPageSupSeven = ['...', list.length];
-  if (list.length <= 7) { return [...startInfSeven, pageSelect, ...lastPageInfSeven]; }
+const startInfSeven = (index: number) => {
+  return range(1, index - 1);
+};
+const lastPageInfSeven = (index: number, page: number) => {
+  return range(index + 1 , page );
+};
+const ellipseStart = () => {
+  return [1, '...'];
+};
+const ellipseEnd = (page: number) => {
+  return ['...', page];
+};
+const  pageUnderSeven = (index: number, page: number) => {
+  return [...startInfSeven(index), '(' + index + ')', ...lastPageInfSeven(index, page)];
+};
 
-  if (index <= 3) { return [...startInfSeven, pageSelect, ...list.slice(index + 1, 5), ...lastPageSupSeven]; }
+const indexNearStart = (index: number, page: number) => {
+  return [...startInfSeven(index), '(' + index + ')', ...range(index + 1, 5), ...ellipseEnd(page)];
+};
+const indexNearEnd = (index: number, page: number) => {
+  return [...ellipseStart(), ...range(page - 4, index - 1), '(' + index + ')', ...lastPageInfSeven(index, page)];
+};
 
-  if (index >= list.length - 4) {
-    return [...startSupSeven, ...list.slice(list.length - 5, index), pageSelect, ...lastPageInfSeven];
+const paginationSeven = (index: number, page: number) => {
+  return [...ellipseStart(), index - 1, '(' + index + ')', index + 1, ...ellipseEnd(page)];
+};
+const setPagination = (index: number, page: number) => {
+  if (page <= 7) { return pageUnderSeven(index, page); }
+
+  if (index <= 4) { return indexNearStart(index, page); }
+
+  if (index >= page - 3) {return indexNearEnd(index, page);
   }
 
-  return [...startSupSeven, list[index - 1], pageSelect, list[index + 1], ...lastPageSupSeven];
+  return paginationSeven(index, page);
 };
 
-
-const getList = (max: number) => {
-  return Array.from({length: max}, (v, k) => k + 1);
+const range = (start: number, end: number) => {
+  return Array(end - start + 1).fill(0).map((_, idx) => start + idx);
 };
+
 
 describe('kata pagination seven', () => {
-  const list5 = getList(5);
-  const list9 = getList(9);
+
   it('should show  a list of page 1 to 5 with page  3 index ', () => {
-    expect(setPagination(list5, 2)).toEqual([1, 2, '(3)', 4, 5]);
+    expect(setPagination(3, 5).join(' ')).toEqual('1 2 (3) 4 5');
   });
   it('should show  a list of page 1 to 5  with page  4 index', () => {
-    expect(setPagination(list5, 3)).toEqual([1, 2, 3, '(4)', 5]);
+    expect(setPagination(4, 5).join(' ')).toEqual('1 2 3 (4) 5');
   });
   it('should show  a list of page 1 to 5 with page  1 index', () => {
-    expect(setPagination(list5, 0)).toEqual(['(1)', 2, 3, 4, 5]);
+    expect(setPagination(1, 5).join(' ')).toEqual('(1) 2 3 4 5');
   });
   it('should show  a list of page 1 to 5 with page  5index', () => {
-    expect(setPagination(list5, 4)).toEqual([1, 2, 3, 4, '(5)']);
+    expect(setPagination(5, 5).join(' ')).toEqual('1 2 3 4 (5)');
   });
   it('should show  a list of page 1 to 7 with page  5 index', () => {
-    expect(setPagination(getList(7), 5)).toEqual([1, 2, 3, 4, 5, '(6)', 7]);
+    expect(setPagination(6, 7).join(' ')).toEqual('1 2 3 4 5 (6) 7');
   });
   it('should show  a list of page 1 to 100 with page  42 index', () => {
-    expect(setPagination(getList(100), 42)).toEqual([1, '...', 42, '(43)', 44, '...', 100]);
+    expect(setPagination(43, 100).join(' ')).toEqual('1 ... 42 (43) 44 ... 100');
   });
   it('should show  a list of page 1 to 9 with page  5 index', () => {
-    expect(setPagination(list9, 4)).toEqual([1, '...', 4, '(5)', 6, '...', 9]);
+    expect(setPagination(5, 9).join(' ')).toEqual('1 ... 4 (5) 6 ... 9');
   });
   it('should show  a list of page 1 to 9 with page  2 index and first page ', () => {
 
-    expect(setPagination(list9, 1)).toEqual([1, '(2)', 3, 4, 5, '...', 9]);
+    expect(setPagination(2, 9).join(' ')).toEqual('1 (2) 3 4 5 ... 9');
   });
   it('should show  a list of page 1 to 9 with page  4 index and first page ', () => {
-    expect(setPagination(list9, 3)).toEqual([1, 2, 3, '(4)', 5, '...', 9]);
+    expect(setPagination(4, 9).join(' ')).toEqual('1 2 3 (4) 5 ... 9');
   });
   it('should show  a list of page 1 to 9 with page  8 index and last page ', () => {
-    expect(setPagination(list9, 7)).toEqual([1, '...', 5, 6, 7, '(8)', 9]);
+    expect(setPagination(8, 9).join(' ')).toEqual('1 ... 5 6 7 (8) 9');
   });
   it('should show  a list of page 1 to 9 with page  6 index and last page ', () => {
 
-    expect(setPagination(list9, 5)).toEqual([1, '...', 5, '(6)', 7, 8, 9]);
+    expect(setPagination(6, 9).join(' ')).toEqual('1 ... 5 (6) 7 8 9');
   });
 });
